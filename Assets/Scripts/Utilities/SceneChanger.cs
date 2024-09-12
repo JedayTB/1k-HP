0ed9cd25ca4c890b1731a63cbd0625799bb54da4
JedayTB <117135725+JedayTB@users.noreply.github.com>
2024-09-12 16:16:16 -0400

    scene switch fix

--- Assets/Scripts/Utilities/SceneChanger.cs
+++ Assets/Scripts/Utilities/SceneChanger.cs
@@ -1,9 +1,15 @@
 using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public static SceneChanger Instance {
+        get { return instance; }
+    }
+
+
     public static void ChangeSceneTo(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
@@ -11,22 +17,55 @@ public class SceneChanger : MonoBehaviour
         Application.Quit();
     }
 
-    
+
+    private static SceneChanger instance;
     [SerializeField] private Animator _anim;
     [SerializeField] private float _transitionTime;
+    [SerializeField] private GameObject[] _canvasElements;
+    private Canvas _canvas;
     void Awake()
     {
         _anim = GetComponent<Animator>();
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            _canvas = GetComponentInParent<Canvas>();
+
+            DontDestroyOnLoad(this.gameObject);
+            DontDestroyOnLoad(_canvas.gameObject);
+        }
+        
     }
+
     public void LoadLevelWithTransition(string sceneName){
         StartCoroutine(StartSceneTransition(sceneName));
     }
 
     IEnumerator StartSceneTransition(string sceneName){
-        _anim.SetTrigger("Start");
+
+        _anim.SetBool("Start", true);
 
         yield return new WaitForSeconds(_transitionTime);
 
+        foreach (var element in _canvasElements) { 
+            element.gameObject.SetActive(false);
+        }
+
         ChangeSceneTo(sceneName);
+
+        StartCoroutine(EndSceneTransition());
+    }
+    IEnumerator EndSceneTransition()
+    {
+        _anim.SetBool("Start", false);
+
+        yield return new WaitForSeconds(_transitionTime);
+
+        _canvas.gameObject.SetActive(false);
+
     }
 }
