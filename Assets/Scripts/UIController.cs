1245a787f39aa11dcf7fd9d79a7d21bd4261d633
JedayTB <117135725+JedayTB@users.noreply.github.com>
2024-09-30 23:53:55 -0400

    Nitro UI Element

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] private GameStateManager _gameStateManager;
     [SerializeField] private GameObject _pauseMenu;
 
+    [SerializeField] private KeyCode _pauseMenuKey = KeyCode.Escape;
+    [SerializeField] private Slider _playerNitroSlider;
     private bool _menuIsOpen = false;
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape)) 
+        if (Input.GetKeyUp(_pauseMenuKey))
         {
             menuOpenClose();
         }
+        
+        _playerNitroSlider.value = _gameStateManager._player._nitroAmount;
     }
 
     private void menuOpenClose()
