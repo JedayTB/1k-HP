3fad0929f4852339942f9eb6e9e203c2be533a34
Etinler <160084549+Etinler@users.noreply.github.com>
2024-10-15 19:57:41 -0400

    camera rotatin g but its so STUPID
    ill figure it out

--- Assets/Scripts/CameraFollower3D.cs
+++ Assets/Scripts/CameraFollower3D.cs
@@ -5,14 +5,21 @@ public class CameraFollower3D : MonoBehaviour
     private Transform _transform;
     [SerializeField] private Transform _target;
     [SerializeField] private Transform _desiredLocation;
+    [SerializeField] private Transform _pivot;
     [SerializeField] private float smoothSpeed = 0.25f;
+    [SerializeField] private float _sensitivity = 1f;
     [SerializeField] private KeyCode _rearViewKey = KeyCode.Tab;
-    
+
     private Vector3 _currentVelocity = Vector3.zero;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    public float rotated;
 
     void Awake()
     {
         _transform = transform;
+        _pivot.transform.localRotation = Quaternion.identity;
     }
 
     private void Update()
@@ -22,8 +29,8 @@ public class CameraFollower3D : MonoBehaviour
 
     private void checkForInput()
     {
-        float horizontalInput = Input.GetAxisRaw("Mouse X");
-        float verticalInput = Input.GetAxisRaw("Mouse Y");
+        _horizontalInput = Input.GetAxisRaw("Mouse X");
+        _verticalInput = Input.GetAxisRaw("Mouse Y");
 
         if (Input.GetKeyDown(_rearViewKey))
         {
@@ -43,11 +50,28 @@ public class CameraFollower3D : MonoBehaviour
     }
 
     void FixedUpdate(){
+        ///
+        /// vector3 currentEulerrotation = camera.rot
+        /// rottate code here 
+        /// currentEulerrotation.y = Mathf.clamp(y, -35, 35);
+        /// camer.rot = currentEul
+        ///
+
+        Vector3 currentEulerRotation = _pivot.transform.localRotation.eulerAngles;
+        print(currentEulerRotation);
+
+        currentEulerRotation.y += -_horizontalInput * _sensitivity;
+
+        _pivot.transform.localRotation = Quaternion.Euler(currentEulerRotation);
+
+
         Vector3 targetPosition = _desiredLocation.position;
 
         _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _currentVelocity, smoothSpeed);
 
         _transform.LookAt(_target);
+
+
 
         // We need to change the rotation of the Z to the car's Z after the LookAt, or else it gets overridden
         Quaternion newRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, _target.rotation.eulerAngles.z);
