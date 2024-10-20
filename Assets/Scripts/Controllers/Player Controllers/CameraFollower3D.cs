33d6538adacba26dc4f888c42a39761a763d3692
Etinler <160084549+Etinler@users.noreply.github.com>
2024-10-20 18:47:21 -0400

    camera actually works now wow 🤯
    you can only rotate it to +-35 degrees (or whatever is set) AND it auto lerps back to 0 if you don't input for x amount of time.
    
    no y yet but i'll do that later

--- Assets/Scripts/Controllers/Player Controllers/CameraFollower3D.cs
+++ Assets/Scripts/Controllers/Player Controllers/CameraFollower3D.cs
@@ -8,18 +8,25 @@ public class CameraFollower3D : MonoBehaviour
     [SerializeField] private Transform _pivot;
     [SerializeField] private float smoothSpeed = 0.25f;
     [SerializeField] private float _sensitivity = 1f;
+    [SerializeField] private float _maximumRotationX = 35f;
+    [SerializeField] private float _timeUntilReset = 1f;
     [SerializeField] private KeyCode _rearViewKey = KeyCode.Tab;
+    [SerializeField] private bool _reverseCameraX = false;
 
     private Vector3 _currentVelocity = Vector3.zero;
     private float _horizontalInput;
     private float _verticalInput;
-
-    public float rotated;
+    private float _timeSinceInput;
+    private bool _lerping = false;
+    private float _startTime;
+    private Vector3 _startRotation;
 
     void Awake()
     {
         _transform = transform;
         _pivot.transform.localRotation = Quaternion.identity;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -49,6 +56,26 @@ public class CameraFollower3D : MonoBehaviour
         }
     }
 
+    private float lerpRotation(Vector3 currentEulerRotation)
+    {
+        if (!_lerping) // i feel like this should be out but it broke when i did that sooo
+        {
+            _startTime = Time.time;
+            _startRotation = currentEulerRotation;
+            _lerping = true;
+        }
+
+        float progress = 1 - Mathf.Pow(1 - ((Time.time - _startTime) - 0.5f), 0.5f);
+        currentEulerRotation.y = Mathf.Lerp(currentEulerRotation.y, 0, progress);
+
+        if (progress == 1f)
+        {
+            _lerping = false;
+        }
+
+        return currentEulerRotation.y;
+    }
+
     void FixedUpdate(){
         ///
         /// vector3 currentEulerrotation = camera.rot
@@ -57,21 +84,41 @@ public class CameraFollower3D : MonoBehaviour
         /// camer.rot = currentEul
         ///
 
-        Vector3 currentEulerRotation = _pivot.transform.localRotation.eulerAngles;
-//        print(currentEulerRotation);
+        // If there has been input, make the time since 0, otherwise add to it
+        _timeSinceInput = _horizontalInput != 0 ? 0 : _timeSinceInput += Time.deltaTime;
+        
+        // It gets real angry if you don't do this and try to move while it's lerping
+        if (_timeSinceInput < _timeUntilReset)
+        {
+            _lerping = false;
+        }
 
+        Vector3 currentEulerRotation = _pivot.transform.localRotation.eulerAngles;
+        _horizontalInput *= _reverseCameraX ? -1 : 1; // oooh my i feel so cool.....
         currentEulerRotation.y += -_horizontalInput * _sensitivity;
 
-        _pivot.transform.localRotation = Quaternion.Euler(currentEulerRotation);
+        // Stupid work around because rotation resets to 359 degrees instead of -1 when going below 0
+        if (currentEulerRotation.y > 180f)
+        {
+            currentEulerRotation.y -= 360f; // i am SUCH a smart cookie
+        }
 
+        // Lerps as long as there hasn't been enough input and the rotation isn't already 0
+        if (_timeSinceInput > _timeUntilReset && currentEulerRotation.y != 0)
+        {
+            currentEulerRotation.y = lerpRotation(currentEulerRotation);
+        }
+
+        currentEulerRotation.y = Mathf.Clamp(currentEulerRotation.y, -_maximumRotationX, _maximumRotationX);
+
+        // Sometimes we get a NaN error here, I did my best to get rid of it but it still shows up sometimes
+        // I did manage to make it never (I think) affect gameplay, though
+        _pivot.transform.localRotation = Quaternion.Euler(currentEulerRotation);
 
         Vector3 targetPosition = _desiredLocation.position;
 
         _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _currentVelocity, smoothSpeed);
-
         _transform.LookAt(_target);
-
-
 
         // We need to change the rotation of the Z to the car's Z after the LookAt, or else it gets overridden
         Quaternion newRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, _target.rotation.eulerAngles.z);
