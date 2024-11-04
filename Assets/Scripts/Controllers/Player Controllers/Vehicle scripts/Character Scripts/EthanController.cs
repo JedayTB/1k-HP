9cc3bd336becc6e0e60c88b9775e315701cfdb2a
JedayTb <ethansuper32@gmail.com>
2024-11-04 18:11:59 -0500

    Lightning Character ability Implemented. Respawns target for now

--- Assets/Scripts/Controllers/Player Controllers/Vehicle scripts/Character Scripts/EthanController.cs
+++ Assets/Scripts/Controllers/Player Controllers/Vehicle scripts/Character Scripts/EthanController.cs
@@ -10,9 +10,8 @@ public class EthanController : PlayerVehicleController
     [SerializeField] private float cubeSize = 0.5f;
     [SerializeField] private LayerMask VehicleLayer;
 
-    private Transform lightningTarget;
+    private I_VehicleController lightningTarget;
     [SerializeField] private Image crosshair;
-
 
     // Update is called once per frame
     public override void Init(InputManager inputManager)
@@ -29,58 +28,73 @@ public class EthanController : PlayerVehicleController
 
     private void getAbiliyTarget()
     {
-        if (_abilityGauge == 100)
+        if (_abilityGauge >= 100)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             Ray aimray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            Debug.DrawRay(aimray.origin, aimray.direction * _maxLightningDistance);
+            if(_isDebuging) Debug.DrawRay(aimray.origin, aimray.direction * _maxLightningDistance);
 
             RaycastHit hitInfo;
             Physics.Raycast(aimray.origin, aimray.direction, out hitInfo, _maxLightningDistance, VehicleLayer);
 
             I_VehicleController vehicleTarget = null;
-            
-            if (hitInfo.collider != null && hitInfo.collider.GetInstanceID() != selfColliderID) {
+
+            if (hitInfo.collider != null && hitInfo.collider.GetInstanceID() != selfColliderID)
+            {
                 //beautiful code
                 //print($"{hitInfo.collider.gameObject.transform.parent.parent.name} id {hitInfo.collider.GetInstanceID()}");
                 vehicleTarget = hitInfo.collider.gameObject.transform.parent.parent.gameObject.GetComponent<I_VehicleController>();
-                
 
-            } 
+            }
 
-            if (vehicleTarget != null) lightningTarget = vehicleTarget.transform;
+            if (vehicleTarget != null) lightningTarget = vehicleTarget;
 
 
-            if (lightningTarget != null) {
-                Vector2 UIMove = Camera.main.WorldToScreenPoint(lightningTarget.position);
+            if (lightningTarget != null)
+            {
+                Vector2 UIMove = Camera.main.WorldToScreenPoint(lightningTarget.transform.position);
                 crosshair.transform.position = UIMove;
                 crosshair.gameObject.SetActive(true);
 
-
-                float distanceToTarget = Vector3.Distance(transform.position, lightningTarget.position);
+                float distanceToTarget = Vector3.Distance(transform.position, lightningTarget.transform.position);
                 if (distanceToTarget > _maxLightningDistance)
                 {
                     lightningTarget = null;
                     crosshair.gameObject.SetActive(false);
                 }
+            }else
+            {
+                crosshair.gameObject.SetActive(false);
             }
-            
-            
-            
-            
         }
-        
+        else
+        {
+            crosshair.gameObject.SetActive(false);
+        }
+
     }
 
     public override void useCharacterAbility()
-    {
-        /*
-        Vector3 lightningDir = transform.forward;
-        Vector3 boxSize = new(cubeSize, cubeSize, cubeSize);
+    {   
+        if (lightningTarget != null && _abilityGauge >= 100)
+        {
 
-        Physics.BoxCast(transform.position, boxSize, lightningDir, Quaternion.identity, _maxLightningDistance, PlayerLayer);
-        */
+            Vector3 lightningDir = lightningTarget.transform.position - Camera.main.transform.position;
+            Vector3 boxSize = new(cubeSize, cubeSize, cubeSize);
+
+            bool hitVehicle = Physics.BoxCast(Camera.main.transform.position, boxSize, lightningDir,
+                                             Quaternion.identity, _maxLightningDistance, VehicleLayer);
+            if(_isDebuging) Debug.DrawRay(Camera.main.transform.position, lightningDir * _maxLightningDistance);
+            if (hitVehicle){
+                onVehicleHit();
+                _abilityGauge = 0;
+            } 
+        }
+    }
+    private void onVehicleHit(){
+        print($"Hit {lightningTarget.name}. Respawning");
+        lightningTarget.respawn();
     }
 }
