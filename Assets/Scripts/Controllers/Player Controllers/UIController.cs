78aad9de2269961dadb9f2cc01c0911114c99843
Etinler <160084549+Etinler@users.noreply.github.com>
2024-11-17 22:08:42 -0500

    Minimap is better AND quick player reset changes  😱
    minimap has an icon now and rotates with the player. also is scaled correctly for the city stage.
    reset now just places the player slightly higher than they were, with a reset to the x rotation. also freezes the rotation for a little bit before landing.
    ALSO BEST CHANGE YOU CAN PRESS R TO RESET NOW!!

--- Assets/Scripts/Controllers/Player Controllers/UIController.cs
+++ Assets/Scripts/Controllers/Player Controllers/UIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Specialized;
 using TMPro;
 using UnityEngine;
@@ -16,18 +17,26 @@ public class UIController : MonoBehaviour
     [SerializeField] private Slider _builtUpNitroSlider;
     [SerializeField] private bool _menuIsOpen = false;
     [SerializeField] private RectTransform _miniPlayerPosition;
-    [SerializeField] private float _miniScaleDivide = 100f;
+    [SerializeField][Tooltip("Changes how fast the minimap icon moves in relation to the players speed. \nHigher value = slower speed.")] private float _miniScaleDivide = 100f;
+    [SerializeField] private float _resetFreezeDuration = 1.5f;
+
+    private Vector3 cachedLocation;
 
     public void init(PlayerVehicleController PLAYER){
         _player = PLAYER; 
         _playerNitroSlider.maxValue = _player.MaxNitroChargeAmounts;
         _builtUpNitroSlider.maxValue = 1f;
+        cachedLocation = _player.transform.position;
     }
     void Update()
     {
         if (Input.GetKeyUp(_pauseMenuKey))
         {
             menuOpenClose();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            resetPlayer();
         }
         _builtUpNitroSlider.gameObject.SetActive(_player.isDrifting);
         if(_player.isDrifting){
@@ -70,8 +79,21 @@ public class UIController : MonoBehaviour
 
     public void resetPlayer()
     {
-        GameStateManager.Player.respawn();
-        menuOpenClose();
+        _player._vehiclePhysics.RigidBody.velocity = Vector3.zero;
+
+        Vector3 newPosition = new Vector3(_player.transform.position.x, _player.transform.position.y + 10f, _player.transform.position.z);
+        _player.transform.position = newPosition;
+
+        Quaternion newRotation = Quaternion.Euler(0, _player.transform.rotation.eulerAngles.y, 0);
+        _player.transform.rotation = newRotation;
+
+        StartCoroutine(FreezeRotation(1.5f));
+
+        //GameStateManager.Player.respawn();
+        if (_menuIsOpen)
+        {
+            menuOpenClose();
+        }
     }
 
     public void continueButton()
@@ -86,9 +108,26 @@ public class UIController : MonoBehaviour
 
     private void miniMap()
     {
-        Vector3 playerVelocity = _player._vehiclePhysics.RigidBody.velocity;
+        // Essentially we are just taking the difference between the current location of the car and the location last frame, and adding that to the location of the minimap icon
+        // Vehicle's z rotation is in the y spot for the minimap icon rotation becuase 2D to 3D silliness
+        Vector3 newLocation = new Vector3((_player.transform.position.x - cachedLocation.x) / _miniScaleDivide, (_player.transform.position.z - cachedLocation.z) / _miniScaleDivide, 0);
+        _miniPlayerPosition.position += newLocation;
+        cachedLocation = _player.transform.position;
 
-        Vector3 minimapUpdate = new Vector3(_miniPlayerPosition.position.x + (playerVelocity.x / _miniScaleDivide * 1.7f), _miniPlayerPosition.position.y + (playerVelocity.z / _miniScaleDivide), _miniPlayerPosition.position.z);
-        _miniPlayerPosition.position = minimapUpdate;
+        // We change the z rotation of the minimap icon according to the vehicles y rotation (because of 2D to 3D shenanigans)
+        Quaternion minimapRotation = Quaternion.Euler(0, 0, -_player.transform.rotation.eulerAngles.y);
+        _miniPlayerPosition.rotation = minimapRotation;
+    }
+
+    public IEnumerator FreezeRotation(float time) // my first coroutine omg are you proud of me :3
+    {
+        float count = 0;
+        _player._vehiclePhysics.RigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+        while (count < time)
+        {
+            count += Time.deltaTime;
+            yield return null;
+        }
+        _player._vehiclePhysics.RigidBody.constraints = RigidbodyConstraints.None;
     }
 }
