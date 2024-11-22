b012404db43ab409d33f2d776b67d79c422a7fbc
JedayTb <ethansuper32@gmail.com>
2024-11-21 19:22:33 -0500

    Fixed Cindy Chilli Oil launching

--- Assets/Scripts/Controllers/Game Controllers/ChiliOilPuddle.cs
+++ Assets/Scripts/Controllers/Game Controllers/ChiliOilPuddle.cs
@@ -3,22 +3,46 @@ using UnityEngine;
 
 public class ChilliOilPuddle : MonoBehaviour
 {
-    [SerializeField] private int OwnerHashCode;
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private BoxCollider BC;
+    [SerializeField] private BoxCollider BCTrigger;
+    [SerializeField] private I_VehicleController Owner;
     [SerializeField] private float speedMultiplier = 1.25f;
     [SerializeField] private float _speedBoostTime = 0.75f;
     [SerializeField] public Rigidbody rb;
-
     I_VehicleController vhc;
+
+
+    void Awake(){
+        BC.enabled = false;
+        BCTrigger.enabled = false;
+    }
     public void Init(PlayerVehicleController owner)
     {
-        OwnerHashCode = owner.GetInstanceID();
+        Owner = owner;
         rb = GetComponent<Rigidbody>();
+    }
+    private void Update(){
+        if(!BC.enabled && !BCTrigger.enabled){
+            bool isGrounded = Physics.Raycast(transform.position, -transform.up, 1f, groundLayers);
+            BC.enabled = isGrounded;
+            BCTrigger.enabled = isGrounded;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.LogWarning($"Getting Component off of {other.gameObject.name}. Inefficient");
+        //More fuck ugly dogshit stupid code
+        //            physics model  actual object with I_VehicleController attached
+        vhc = other.transform.parent.parent.gameObject.GetComponent<I_VehicleController>();
+        if(vhc != null && vhc == Owner){
+            print("Boosting Owner speed!");
+            StartCoroutine(increaseOwnerSpeed(_speedBoostTime, speedMultiplier, vhc));
+            vhc = null;
+        }
+        print($"{other.gameObject.name}");
     }
-    IEnumerator increaseOwnerSpeed(float time, PlayerVehicleController owner)
+    IEnumerator increaseOwnerSpeed(float time,float speedMultiplier, I_VehicleController owner)
     {
         float count = 0f;
         float baseAccel = owner.VehiclePhysics.Acceleration;
