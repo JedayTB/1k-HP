95bae7d079bbc079e9c983584f68a0f43ad4d4bd
Ethan Ambler <Eambler7709@conestogac.on.ca>
2025-01-21 14:46:35 -0500

    Blob shadow start

--- Assets/Scripts/Controllers/Game Controllers/GrandPrixManager.cs
+++ Assets/Scripts/Controllers/Game Controllers/GrandPrixManager.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class GrandPrixManager
 {
     public static int GameMode = 0; // 0 is Grand Prix, 1 is Level Select
-    public static string[] LevelOrder = {"CityLevel", "MountainLevel", "PastelLevel", "JapanLevel"};
-    public static string[] LevelDisplayNames = { "City", "Mountain", "Pastel City", "Japan"};
-    public static int[] RacePlacements = { 0, 0, 0, 0 };
+    public static string[] LevelOrder = {"CityLevel", "PastelLevel"};
+    public static string[] LevelDisplayNames = { "City", "Pastel City"};
+    public static int[] RacePlacements = { 0, 0 };
     public static int CurrentLevelIndex = 0;
-    public static int GrandPrixLength = 4;
+    public static int GrandPrixLength = 2;
 
     public static void SetRacePlacement(int raceIndex, int racePlacement)
     {
