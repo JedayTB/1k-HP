3e8433a57a96d43f1552c58ccf5e6000ed60475d
Dankyss <drg@thedrg.ca>
2025-02-10 23:20:18 -0500

    the great mp purge

--- Assets/Scripts/Controllers/Player Controllers/Main Menu Scripts/MainMenuUI.cs
+++ Assets/Scripts/Controllers/Player Controllers/Main Menu Scripts/MainMenuUI.cs
@@ -1,10 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    [SerializeField]
+    private Animator animator;
+
+    [Header("Video Settings Dropdowns")]
+    [SerializeField]
+    private TMP_Dropdown presetDropdown;
+
+    [SerializeField]
+    private TMP_Dropdown textureDropdown,
+        aaDropdown,
+        fpsDropdown;
 
     public void StartButton()
     {
@@ -15,7 +24,7 @@ public class MainMenuUI : MonoBehaviour
     {
         // no options menu implemented yet
     }
-    
+
     public void ExitButton()
     {
         Application.Quit();
@@ -26,5 +35,27 @@ public class MainMenuUI : MonoBehaviour
         animator.SetBool("MenuIsOpen", false);
     }
 
-   
+    #region Video Settings
+
+    public void ChangePreset() { }
+
+    public void ChangeTexture() { }
+
+    public void ChangeAA()
+    {
+        int[] aaLevels = { 0, 2, 4, 8 };
+        QualitySettings.SetQualityLevel(aaLevels[aaDropdown.value], true);
+    }
+
+    public void ChangeFPS()
+    {
+        int[] fpsLevels = { 30, 60, 120, 144, 240, 0 };
+        Application.targetFrameRate = fpsLevels[fpsDropdown.value];
+    }
+
+    #endregion
+
+    #region Audio Settings
+
+    #endregion
 }
