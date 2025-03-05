6c42da0dac375a881796d351ee6178e7fceefc63
JedayTB <117135725+JedayTB@users.noreply.github.com>
2025-03-05 18:27:35 -0500

    Nitro max Speed + mimi bubble tweak

--- Assets/Scripts/Controllers/Player Controllers/Vehicle scripts/Character Scripts/BubblegumController.cs
+++ Assets/Scripts/Controllers/Player Controllers/Vehicle scripts/Character Scripts/BubblegumController.cs
@@ -3,57 +3,59 @@ using UnityEngine;
 
 public class BubblegumController : A_Ability
 {
-  [Header("Mimi Bubble Specifics")]
-  [SerializeField] private float _BubbleActiveTime = 5f;
-  [SerializeField] private float _bounceStrength = 40;
-  [SerializeField] private float _speedIncreaseAmt = 1.5f;
-  [SerializeField] private float _driftChargeIncreaseAmt = 1.4f;
-  [SerializeField] GameObject _bubble;
+    [Header("Mimi Bubble Specifics")]
+    [SerializeField] private float _BubbleActiveTime = 5f;
+    [SerializeField] private float _bounceStrength = 40;
+    [SerializeField] private float _speedIncreaseAmt = 1.5f;
+    [SerializeField] private float _driftChargeIncreaseAmt = 1.4f;
+    [SerializeField] GameObject _bubble;
 
-  public override void AbilityUsed()
-  {
-    print("Used bubblegum!");
-    StartCoroutine(bubbleActive(_BubbleActiveTime));
-  }
-  IEnumerator bubbleActive(float bubbleActiveTime)
-  {
-    print("bubble func being run");
-    float count = 0;
-
-    _bubble.SetActive(true);
-    vehicle._builtUpNitroAmount *= _driftChargeIncreaseAmt;
-    vehicle.VehiclePhysics.horsePower *= _speedIncreaseAmt;
-
-    while (count < bubbleActiveTime)
+    public override void AbilityUsed()
     {
-      count += Time.deltaTime;
-
-      yield return null;
+        print("Used bubblegum!");
+        StartCoroutine(bubbleActive(_BubbleActiveTime));
+        vehicle.delistAbilityAction(onAbility);
     }
-    _bubble.SetActive(false);
-    print("bubble func ended");
-
-    vehicle._builtUpNitroAmount /= _driftChargeIncreaseAmt;
-    vehicle.VehiclePhysics.horsePower /= _speedIncreaseAmt;
-
-    gameObject.SetActive(false);
-  }
-
-  //Should be from the capsule
-  void OnTriggerEnter(Collider other)
-  {
-
-    A_VehicleController vehicle = other.GetComponentInParent<A_VehicleController>();
-
-    if (vehicle != null)
+    IEnumerator bubbleActive(float bubbleActiveTime)
     {
-      bounceOff(vehicle);
+        print("bubble func being run");
+        float count = 0;
+
+        _bubble.SetActive(true);
+        print($"HP before {vehicle.VehiclePhysics.horsePower}");
+        vehicle._builtUpNitroAmount *= _driftChargeIncreaseAmt;
+        vehicle.VehiclePhysics.horsePower *= _speedIncreaseAmt;
+        print($"HP after {vehicle.VehiclePhysics.horsePower}");
+        while (count < bubbleActiveTime)
+        {
+            count += Time.deltaTime;
+
+            yield return null;
+        }
+        _bubble.SetActive(false);
+        print("bubble func ended");
+
+        vehicle._builtUpNitroAmount /= _driftChargeIncreaseAmt;
+        vehicle.VehiclePhysics.horsePower /= _speedIncreaseAmt;
+
+        gameObject.SetActive(false);
     }
-  }
-  private void bounceOff(A_VehicleController vehicle)
-  {
-    Vector3 dirToBounce = vehicle.transform.position - transform.position;
-    float vehicleWeight = vehicle.VehiclePhysics.RigidBody.mass;
-    vehicle.VehiclePhysics.RigidBody.AddForce(_bounceStrength * vehicleWeight * dirToBounce);
-  }
+
+    //Should be from the capsule
+    void OnTriggerEnter(Collider other)
+    {
+
+        A_VehicleController vehicle = other.GetComponentInParent<A_VehicleController>();
+
+        if (vehicle != null)
+        {
+            bounceOff(vehicle);
+        }
+    }
+    private void bounceOff(A_VehicleController vehicle)
+    {
+        Vector3 dirToBounce = vehicle.transform.position - transform.position;
+        float vehicleWeight = vehicle.VehiclePhysics.RigidBody.mass;
+        vehicle.VehiclePhysics.RigidBody.AddForce(_bounceStrength * vehicleWeight * dirToBounce);
+    }
 }
