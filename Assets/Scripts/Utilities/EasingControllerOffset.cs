8c6d03cd843f5638672dec85ae99a46624a47c3c
JedayTb <ethansuper32@gmail.com>
2025-03-20 22:14:37 -0400

    lil Character select changes - DONT MAKE RADIAL FILLS

--- Assets/Scripts/Utilities/EasingControllerOffset.cs
+++ Assets/Scripts/Utilities/EasingControllerOffset.cs
@@ -1,18 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EasingControllerOffset : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] private float minimumTimeOffset = 0f;
+  [SerializeField] private float maximumTimeOffset = 0.75f;
 
-    // Update is called once per frame
-    void Update()
+  [SerializeField] private float minLerpTime = 5f;
+  [SerializeField] private float maxLerpTime = 15f;
+
+  EasingController eas;
+  void Awake()
+  {
+    eas = GetComponent<EasingController>();
+    eas.enabled = false;
+
+    eas.durationInSeconds = Random.Range(minLerpTime, maxLerpTime);
+    StartCoroutine(countDown());
+  }
+  IEnumerator countDown()
+  {
+    float count = 0f;
+
+    float waitTime = Random.Range(minimumTimeOffset, maximumTimeOffset);
+
+    while (count < waitTime)
     {
-        
+      count += Time.deltaTime;
+      yield return null;
     }
+    eas.enabled = true;
+  }
 }
