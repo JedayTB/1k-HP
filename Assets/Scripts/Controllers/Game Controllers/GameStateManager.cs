688e055ef11e5ea2629fd31e2c59e679cce0ff5d
JedayTB <117135725+JedayTB@users.noreply.github.com>
2025-04-01 23:48:12 -0400

    Wall clip improvement

--- Assets/Scripts/Controllers/Game Controllers/GameStateManager.cs
+++ Assets/Scripts/Controllers/Game Controllers/GameStateManager.cs
@@ -70,7 +70,7 @@ public class GameStateManager : MonoBehaviour
 
     public static PlayerVehicleController Player; // Singleton var
 
-    public static int _newCharacter = 1;
+    public static int _newCharacter = 2;
 
 
     // Hidden
